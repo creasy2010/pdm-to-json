8f5c6b7ea1464f559cf878748a323afa202b8e73
yangxiaodong <yangxiaodong@qianmi.com>
2019-04-19 14:16:38 +0800

    add: 添加 ts类型定义

--- src/types.d.ts
+++ src/types.d.ts
@@ -19,8 +19,98 @@ declare module namespace {
 
   export interface OModel{
     $:Key,
+    "a:ObjectID": string[];
+    "a:Name": string[];
+    "a:Code": string[];
+    "a:CreationDate":string[];
+    "a:Creator":string[];
+    "a:ModificationDate":string[];
+    "a:Modifier": string[];
+    "a:PackageOptionsText":string[];
+    "a:ModelOptionsText":string[];
+    "a:ExtendedAttributesText":string[];
+    "c:DBMS":OShortcut[];
+    "c:Tables":{
+      "o:Table":OTable[];
+    };
+    //TODO
+    "c:PhysicalDiagrams":{"o:PhysicalDiagram":OPhysicalDiagram[]}[];
+    "c:DefaultDiagram":{
+      "o:PhysicalDiagram":KeyRef[],
+    };
+    "c:Users":{
+      "o:User":OUser[]
+    }[];
+    "c:References":{
+      "o:Reference":[]
+    }[];
+    "c:DefaultGroups":null;
+    "c:ChildTraceabilityLinks":null;
+    "c:TargetModels":null;
   }
 
+
+  export interface OUser {
+    $: Key;
+    "a:ObjectID": string[];
+    "a:Name": string[];
+    "a:Code": string[];
+    "a:CreationDate": string[];
+    "a:Creator": string[];
+    "a:ModificationDate": string[];
+    "a:Modifier": string[];
+  }
+
+
+  export interface OTableSymbol {
+    $: Key;
+    "a:ModificationDate": string[];
+    "a:IconMode": string[];
+    "a:Rect": string[];
+    "a:LineColor": string[];
+    "a:FillColor": string[];
+    "a:ShadowColor": string[];
+    "a:FontList": string[];
+    "a:BrushStyle": string[];
+    "a:GradientFillMode": string[];
+    "a:GradientEndColor": string[];
+    "c:Object": {"o:Table":{$:KeyRef}}[];
+    "a:CreationDate": string[];
+  }
+
+  export interface OPhysicalDiagram {
+    $: Key;
+    "a:ObjectID": string[];
+    "a:Name": string[];
+    "a:Code": string[];
+    "a:CreationDate": string[];
+    "a:Creator": string[];
+    "a:ModificationDate": string[];
+    "a:Modifier": string[];
+    "a:DisplayPreferences": string[];
+    "a:PaperSize": string[];
+    "a:PageMargins": string[];
+    "a:PaperSource": string[];
+    "c:Symbols": {
+      "o:TableSymbol": OTableSymbol[];
+    }[];
+  }
+
+
+
+  export interface OShortcut {
+    $: Key;
+    "a:ObjectID": string[];
+    "a:Name": string[];
+    "a:Code": string[];
+    "a:CreationDate": string[];
+    "a:Creator": string[];
+    "a:ModificationDate": string[];
+    "a:Modifier": string[];
+    "a:TargetStereotype": string[];
+    "a:TargetID": string[];
+    "a:TargetClassID": string[];
+  }
 
 
 
@@ -40,8 +130,6 @@ declare module namespace {
     "c:Owner": COwner[];
     "c:PrimaryKey": CPrimaryKey[];
   }
-
-
 
 
   export interface OColumn {
@@ -64,10 +152,6 @@ declare module namespace {
   export interface CColumn {
     "o:Column": OColumn[];
   }
-//
-// export interface 4 {
-//     Ref: string;
-//   }
 
   export interface KeyRef {
     $: {
@@ -96,16 +180,10 @@ declare module namespace {
     "o:Key": OKey[];
   }
 
-// export interface 5 {
-//     Ref: string;
-//   }
-
-  export interface OUser {
-    $: KeyRef;
-  }
-
   export interface COwner {
-    "o:User": OUser[];
+    "o:User": {
+      $ : KeyRef;
+    }[];
   }
 
   export interface OKey2 {
